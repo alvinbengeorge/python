47be4b692ade9b05784e0fc2dc44fc15ef5fb043
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-03-11 08:49:58 +0530

    Piano tiles

---
+++ piano tiles.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+while True:
+    #put your own coordinates
+    if pyautogui.pixel(510,480)[0]==39:
+        pyautogui.click(510,490)
+    if pyautogui.pixel(610,480)[0]==39:
+        pyautogui.click(610,490)
+    if pyautogui.pixel(710,480)[0]==39:
+        pyautogui.click(710,490)
+    if pyautogui.pixel(810,480)[0]==39:
+        pyautogui.click(810,490)
