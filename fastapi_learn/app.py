059f3e66bf086877a94437e909ec7add7c08d829
alvinbengeorge <alvinallen333@gmail.com>
2022-09-03 16:55:16 +0530

    Update fastapi

--- fastapi_learn/app.py
+++ fastapi_learn/app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
+
+class Message(BaseModel):
+    author: str
+    message: str
 
 app = FastAPI(debug=True)
 messages: dict = {}
@@ -7,5 +12,10 @@ messages: dict = {}
 @app.get("/tweets")
 async def tweets():
     return messages
+
+@app.post("/tweet")
+async def tweet(message: Message):
+    messages[message.author] = message.message
+    return "Done"
 
 uvicorn.run(app, host="127.0.0.1", port=8080)
