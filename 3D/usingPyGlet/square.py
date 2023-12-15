4fab7cca2ca102ead7d15d8ec62c1b3eda9def5d
alvinbengeorge <alvinallen333@gmail.com>
2023-12-15 13:20:22 +0530

    batches

--- 3D/usingPyGlet/square.py
+++ 3D/usingPyGlet/square.py
@@ -1,11 +1,27 @@
 from pyglet import shapes
+from pyglet.graphics import Batch
 from random import randint
-from multiprocessing import Pool
+from time import time
 
-def square(x: int, y: int) -> shapes.Rectangle: 
-    return shapes.Rectangle(x, y, 50, 50, color=(randint(0, 255), 0, 0))
+SIZE = 10
+
+def square(x: int, y: int, batch = None) -> shapes.Rectangle: 
+    return shapes.Rectangle(x, y, SIZE, SIZE, color=(randint(0, 255), 0, 0), batch=batch)
 
 def gridGenerator(window):
-    for x in range(0, int(window.width), 50):
-        for y in range(0, int(window.height), 50):
-            square(x, y).draw()
+    start = time()
+    for x in range(0, int(window.width)-int(SIZE//2), SIZE):
+        batch = Batch()
+        squares = columnGeneration(window, x, batch=batch)
+        # print(squares)
+        batch.draw()
+    print(f"Time taken: {time()-start}")
+
+def columnGeneration(window, x, batch):
+    squares = []
+    for y in range(0, int(window.height)-int(SIZE//2), SIZE):
+        squares.append(square(x, y, batch=batch))
+    return squares
+
+if __name__ == "__main__":
+    import main
